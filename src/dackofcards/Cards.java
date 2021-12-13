fdd9cde73b201cc9c90b7bc7afbaf92b23c825ed
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-13 09:08:54 +0530

    Added: Shuffle the cards using Random Method

--- src/dackofcards/Cards.java
+++ src/dackofcards/Cards.java
@@ -1,7 +1,40 @@
 package dackofcards;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Cards {
+    // Declare variables
+    String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9","10", "Jack", "Queen", "King"};
+    String cards[][] = new String[4][13];
+    String index[] = new String[52];
+    int position = 0;
+    //Create Card Deck
+    public void createDeck(){
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<13;j++)
+            {
+                index[position] = suits[i]+" of "+ranks[j];
+                position++;
+            }
+        }
+        System.out.println();
+    }
+    //Shuffle Cards and Display
+    public void shuffleCards(){
+        Collections.shuffle(Arrays.asList(index));
+        for (int a=0;a<index.length;a++){
+            System.out.println("Shuffle: "+index[a]);
+        }
+        System.out.println();
+    }
+    //Main method
     public static void main(String[] args) {
         System.out.println(" welcome to Dack of card game");
+        Cards deckOfCards=new Cards();
+        deckOfCards.createDeck();
+        deckOfCards.shuffleCards();
     }
 }
